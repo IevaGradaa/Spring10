c5422e69ceeccb8470bdd665adc88dd98e481b62
IevaGradaa <61150265+IevaGradaa@users.noreply.github.com>
2020-04-08 15:28:51 +0300

    Add files via upload

---
+++ complete/src/main/java/hello/repository/CustomerRepository.java
@@ -0,0 +1,21 @@
+package hello.repository;
+
+import java.util.List;
+
+import hello.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findAll();
+
+    //TODO: Add code that will be necessary to implement all methods in service
+    List<Customer> findByLastName(String lastName);
+    List<Customer> findByFirstName(String firstName);
+    Customer findCustomerById(Long id);
+    List<Customer> findBySearchKey(String searchKey);
+    Customer save(Customer customer);
+    List<Customer> findByFirstNameAndLastName(String firstName, String lastName);
+
+}
